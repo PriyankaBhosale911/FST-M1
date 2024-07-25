dc18862c120f23c374e051e7df9ae6649c248ce9
PriyankaBhosale911 <priyanka.bhosale@ibm.com>
2024-07-25 20:54:27 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,11 @@
+#name="Priyanka Bhosale"
+name=input("Enter your name: ")
+print("Name is: "+name)
+age=int(input("Enter age: "))
+print("Age in 2024 is: {}".format(age))
+#age="28"
+#Calculate the year they will turn 100
+year= str((2024 - age)+100)
+print("{}turns 100 in te year {} :" + year)
+#print("Age after 30 pri
+# years:"+(age+100))PRI
