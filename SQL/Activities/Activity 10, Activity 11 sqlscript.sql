958e96efcbfc03a34da69e749c38723c1bb75268
PriyankaBhosale911 <priyanka.bhosale@ibm.com>
2024-08-23 17:12:10 +0530

    Add files via upload

---
+++ SQL/Activities/Activity 10, Activity 11 sqlscript.sql
@@ -0,0 +1,31 @@
+REM   Script: Activity 10-11
+REM   Activity 10 and Activity 11
+
+SELECT * FROM orders 
+WHERE salesman_id=(SELECT DISTINCT salesman_id FROM orders WHERE customer_id=3007);
+
+SELECT * FROM orders 
+WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE salesman_city='New York');
+
+SELECT grade, COUNT(*) FROM customers 
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE salesman_city='New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders 
+WHERE salesman_id IN( SELECT salesman_id FROM salesman 
+WHERE commission=( SELECT MAX(commission) FROM salesman));
+
+SELECT customer_id, customer_name FROM customers a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION 
+SELECT salesman_id, salesman_name FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY customer_name;
+
+SELECT a.salesman_id, a.salesman_name, b.order_no, 'highest on', b.order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, a.salesman_name, b.order_no, 'lowest on', b.order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
