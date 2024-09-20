ba240ffaa34f9c3f7ce68273d4ada29049048f55
Geetha-padmanabha <geetha.p@ibm.com>
2024-09-20 14:44:11 +0530

    Update Activity6.java

--- Appium/Activities/Activity6.java
+++ Appium/Activities/Activity6.java
@@ -1,69 +1,72 @@
-package Activities;
+package Project;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.AfterClass;
-import io.appium.java_client.MobileBy;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.Test;
+
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import java.time.Duration;
+
+import static Project.ActionsBase.doSwipe;
 
 public class Activity6 {
-    AndroidDriver<MobileElement> driver;
+    AndroidDriver driver;
     WebDriverWait wait;
-
     @BeforeClass
-    public void beforeClass() throws MalformedURLException {
-        // Set the Desired Capabilities
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "<Your device name>");
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("automationName", "UiAutomator2");
-        caps.setCapability("appPackage", "com.android.chrome");
-        caps.setCapability("appActivity", "com.google.android.apps.chrome.Main");
-        caps.setCapability("noReset", true);
+    public void setUp() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.chrome");
+        options.setAppActivity("com.google.android.apps.chrome.Main");
+        options.noReset();
 
-        // Instantiate Appium Driver
-        URL appServer = new URL("http://0.0.0.0:4723/wd/hub");
-        driver = new AndroidDriver<>(appServer, caps);
-        wait = new WebDriverWait(driver, 5);
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
 
-        // Navigate to the page
-        driver.get("https://www.training-support.net/selenium/lazy-loading");
+        driver = new AndroidDriver(serverURL, options);
+
+
+        driver.get("https://v1.training-support.net/selenium");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.Button[@text=\"Get Started!\"]")));
+        Dimension dims = driver.manage().window().getSize();
+        Point start = new Point((int)(dims.getWidth()*0.5),(int)(dims.getHeight()*0.8));
+        Point end = new Point((int)(dims.getWidth()*0.5),(int)(dims.getHeight()*0.6));
+
+        doSwipe(driver, start, end, 50);
     }
 
-    @Test
-    public void imageScrollTest() throws InterruptedException {
-        // wait for page to load
-        MobileElement pageTitle = driver.findElementByXPath("//android.view.View[@text=\"Lazy Loading\"]");
-        wait.until(ExpectedConditions.textToBePresentInElement(pageTitle, "Lazy Loading"));
-
-        // Count the number of images shown on the screen
-        List<MobileElement> numberOfImages = driver.findElementsByXPath("//android.view.View/android.view.View/android.widget.Image");
-        System.out.println("Number of image shown currently: " + numberOfImages.size());
-        Thread.sleep(2000);
-
-        // Assertion before scrolling
-        Assert.assertEquals(numberOfImages.size(), 2);
-
-        // Scroll to Helen's post
-        driver.findElement(MobileBy.AndroidUIAutomator("UiScrollable(UiSelector()).scrollTextIntoView(\"helen\")"));
-
-        // Find the number of images shown after scrolling
-        numberOfImages = driver.findElementsByXPath("//android.view.View/android.view.View/android.widget.Image");
-        System.out.println("Number of image shown currently: " + numberOfImages.size());
-
-        // Assertion after scrolling
-        Assert.assertEquals(numberOfImages.size(), 4);
+    @Test(priority = 1)
+    public void positiveTest(){
+        loginFormWithUsernamePassword("admin","password");
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text=\"Welcome Back, admin\"]")));
+        Assert.assertTrue(driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Welcome Back, admin\"]")).isDisplayed());
+    }
+    @Test(priority = 2)
+    public void negativeTest(){
+        driver.navigate().back();
+        loginFormWithUsernamePassword("user","password");
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text=\"Invalid Credentials\"]")));
+        Assert.assertTrue(driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Invalid Credentials\"]")).isDisplayed());
     }
 
-    @AfterClass
-    public void afterClass() {
-        driver.quit();
+    public void loginFormWithUsernamePassword(String username, String password)  {
+        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@text=\"Popups\"]")));
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Popups\"]")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.Button[@text=\"Sign in\"]")));
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[@text=\"Sign in\"]")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.EditText[2]")));
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText[1]")).sendKeys(username);
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText[2]")).sendKeys(password);
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[@text=\"Log in\"]")).click();
     }
 }
